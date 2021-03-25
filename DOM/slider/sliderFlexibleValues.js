5248a3865359f8fea7dda2e38daa43bccc34704c
lebedevpa <p.lebedev88@gmail.com>
2021-03-25 07:33:27 +0200

    FlexibleValues done

--- DOM/slider/sliderFlexibleValues.js
+++ DOM/slider/sliderFlexibleValues.js
@@ -9,23 +9,30 @@ const itemWidth = getComputedStyle(items[0]).width;
 const step = parseInt(itemWidth);
 const preShownItems = 300 / step;
 const maxRight = (items.length - preShownItems) * step;
+
+
 let currentRight = 0;
 
-itemsList.style.right = currentRight;
+itemsList.style.right = currentRight; 
 
-right.addEventListener("click", e => {
+right.addEventListener ("click", e => {
+
   e.preventDefault();
+
   if (currentRight < maxRight) {
     currentRight += step;
     itemsList.style.right = `${currentRight}px`;
+
   }
+
 })
 
-left.addEventListener("click", e => {
+left.addEventListener ("click", e => {
   e.preventDefault();
 
   if (currentRight > minRight) {
     currentRight -= step;
     itemsList.style.right = `${currentRight}px`;
+
   }
 })
