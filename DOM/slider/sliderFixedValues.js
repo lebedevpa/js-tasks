3db5ab8d7748b9221a93b090a3c08b55f8085583
lebedevpa <p.lebedev88@gmail.com>
2021-03-25 07:45:43 +0200

    domManipulations done

--- DOM/slider/sliderFixedValues.js
+++ DOM/slider/sliderFixedValues.js
@@ -1,22 +1,41 @@
-const left = document.querySelector("#left");
-  const right = document.querySelector("#right");
-  const items = document.querySelector("#items");
-  const computed = window.getComputedStyle(items);
+const leftBtn = document.querySelector("#left");
+const rightBtn = document.querySelector("#right");
+const itemsList = document.querySelector("#items");
+const computedStyles = getComputedStyle(items);
 
-  right.addEventListener("click", function(e) {
-    e.preventDefault();
-    let currentRight = parseInt(computed.right);
 
-    if (currentRight < 500) {
-      items.style.right = currentRight + 100 + "px";
-    }
-  });
+const loop = (direction, e) => {
+  e.preventDefault();
 
-  left.addEventListener("click", function(e) {
-    e.preventDefault();
-    let currentRight = parseInt(computed.right);
+  if (direction === "right") {
+    itemsList.appendChild(itemsList.firstElementChild);
+  } else {
+    itemsList.insertBefore(itemsList.lastElementChild, items.firstElementChild);
+  }
+};
 
-    if (currentRight > 0) {
-      items.style.right = currentRight - 100 + "px";
-    }
-  });
+rightBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let currentRight = parseInt(computedStyles.right);
+
+  if (currentRight < 500) {
+    items.style.right = `${currentRight + 100}px`;
+  }
+
+
+
+});
+
+leftBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let currentRight = parseInt(computedStyles.right);
+
+  if (currentRight > 0) {
+    items.style.right = `${currentRight - 100}px`;
+  }
+
+
+
+});
